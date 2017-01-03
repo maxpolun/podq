cf77f069db8c65088f4c0ab66840ce411bc791b2
Max Polun <orrerey@gmail.com>
2017-01-03 10:07:30 -0500

    better opbeat support

--- server/main.ts
+++ server/main.ts
@@ -1,6 +1,7 @@
 import {port, dbUrl} from '../config'
 import * as Koa from 'koa'
 import {pgp} from './pg'
+import * as KoaRouter from 'koa-router'
 
 if (process.env.OPBEAT_APP_ID) {
   var opbeat = require('opbeat').start()
@@ -8,10 +9,19 @@ if (process.env.OPBEAT_APP_ID) {
 
 let app = new Koa()
 let db = pgp(dbUrl)
+let router = new KoaRouter()
 
-app.use(async ctx => {
+router.get('/', async ctx => {
   ctx.body = (await db.one('SELECT uuid_generate_v4();')).uuid_generate_v4
 })
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+if (opbeat) {
+  app.use(async (ctx, next) => {
+    opbeat.setTransactionName(ctx.request.method + ' ' + (ctx as any)._matchedRoute)
+  })
+}
 
 app.listen(port, () => {
   console.log(`server started on port ${port}`)
